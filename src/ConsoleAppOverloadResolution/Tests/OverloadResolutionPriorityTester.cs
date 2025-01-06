4b9aa402ee85fc83613403433ba2454c8b677b5e
Renato Groffe <renato.groffe@yahoo.com.br>
2025-01-06 11:51:58 -0300

    Update OverloadResolutionPriorityTester.cs

--- src/ConsoleAppOverloadResolution/Tests/OverloadResolutionPriorityTester.cs
+++ src/ConsoleAppOverloadResolution/Tests/OverloadResolutionPriorityTester.cs
@@ -1,20 +1,20 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleAppOverloadResolution.Tests;
 
 public class OverloadResolutionPriorityTester
 {
-    [OverloadResolutionPriority(2)]
+    [OverloadResolutionPriority(1)]
     public static void PrintItems(params string[] items)
        => Console.WriteLine($"{nameof(OverloadResolutionPriorityTester)} - " +
            $"string[]: [ {String.Join(" | ", items)} ]");
 
-    [OverloadResolutionPriority(1)]
+    [OverloadResolutionPriority(2)]
     public static void PrintItems(params ReadOnlySpan<string?> items)
        => Console.WriteLine($"{nameof(OverloadResolutionPriorityTester)} - " +
            $"ReadOnlySpan<string?>: [ {String.Join(" | ", items)} ]");
 
-    // Assume [OverloadResolutionPriority(0)]
+    [OverloadResolutionPriority(3)]
     public static void PrintItems(params IEnumerable<string> items)
        => Console.WriteLine($"{nameof(OverloadResolutionPriorityTester)} - " +
            $"IEnumerable<string>: [ {String.Join(" | ", items)} ]");
